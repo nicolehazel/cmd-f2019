d595adba1fbf7203e4d9fbfb24a0abfbcb57a979
nattiechan <nattie.chan@gmail.com>
2019-03-10 12:06:54 -0700

    added database query

--- app/src/main/java/com/example/cmd_f2019app/ScanBarcode.java
+++ app/src/main/java/com/example/cmd_f2019app/ScanBarcode.java
@@ -12,6 +12,10 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ScanBarcode extends AppCompatActivity implements OnClickListener {
 
@@ -22,15 +26,15 @@ public class ScanBarcode extends AppCompatActivity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_barcode);
-        scanBtn = (Button)findViewById(R.id.scan_button);
-        formatTxt = (TextView)findViewById(R.id.scan_format);
-        contentTxt = (TextView)findViewById(R.id.scan_content);
+        scanBtn = (Button) findViewById(R.id.scan_button);
+        formatTxt = (TextView) findViewById(R.id.scan_format);
+        contentTxt = (TextView) findViewById(R.id.scan_content);
         scanBtn.setOnClickListener(this);
     }
 
-    public void onClick(View v){
-    //respond to clicks
-        if(v.getId()==R.id.scan_button){
+    public void onClick(View v) {
+        //respond to clicks
+        if (v.getId() == R.id.scan_button) {
             //scan
             IntentIntegrator scanIntegrator = new IntentIntegrator(this);
             scanIntegrator.initiateScan();
@@ -38,19 +42,68 @@ public class ScanBarcode extends AppCompatActivity implements OnClickListener {
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-    //retrieve scan result
+        //retrieve scan result
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanningResult != null) {
             //we have a result
             String scanContent = scanningResult.getContents();
             String scanFormat = scanningResult.getFormatName();
+
+            String result = db_query("63243275779");
+
             formatTxt.setText("FORMAT: " + scanFormat);
-            contentTxt.setText("CONTENT: " + scanContent);
-        }
-        else{
+            contentTxt.setText("CONTENT: " + result);
+
+
+        } else {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
         }
     }
-}
+
+    public String db_query(String scanContent){
+
+        try
+            {
+                // create our mysql database connection
+                String myDriver = "org.gjt.mm.mysql.Driver";
+                String myUrl = "jdbc:mysql://localhost:8080";
+                Class.forName(myDriver);
+                Connection conn = DriverManager.getConnection(myUrl, "root", "");
+
+                // our SQL SELECT query.
+                // if you only need a few columns, specify them by name instead of using "*"
+                String query = "SELECT option_1,option_2 FROM `db` WHERE UPC = ${scanContent}";
+
+                // create the java statement
+                Statement st = conn.createStatement();
+
+                // execute the query, and get a java resultset
+                ResultSet rs = st.executeQuery(query);
+
+                // iterate through the java resultset
+                while (rs.next())
+                {
+                    String option_1 = rs.getString("option_1");
+                    String option_2 = rs.getString("option_2");
+
+                    String output = option_1.concat(",").concat(option_2);
+
+
+                    // print the results
+                    return output;
+                }
+                st.close();
+            }
+            catch (Exception e)
+            {
+                System.err.println("Got an exception! ");
+                System.err.println(e.getMessage());
+            }
+
+        String warning = "Error";
+        return warning;
+
+        }
+    }
